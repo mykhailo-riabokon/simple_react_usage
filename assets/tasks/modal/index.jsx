475f2735fadaf4273c7fa9f4c1356ee1c5d8bda1
evilbobul <lesykmagula@gmail.com>
2016-07-27 02:02:13 +0300

    still bullshit

--- assets/tasks/modal/index.jsx
+++ assets/tasks/modal/index.jsx
@@ -4,22 +4,45 @@ class App extends React.Component {
 
     this.state = {
       isShown: false
-    };
+    }
 
-    this.showModal = this.showModal.bind(this);
+    this.onClickChange = this.onClickChange().bind(this);
   }
 
-  showModal() {
-    this.setState({ showModal: !this.state.isShown });
+  onClickChange(){
+    this.setState({isShown: !this.state.isShown})
   }
 
   render() {
     return (
       <div className="ui container">
         <h1>This is React Semantic UI modal</h1>
-        <button onClick={this.showModal}>Show modal</button>
+        <button onClick={this.onClickChange}>Show modal</button>
         <Modal isShown={this.state.isShown}>
-          <h1>Some modal content</h1>
+            <i className="close icon"></i>
+            <div className="header">
+              Archive Old Messages
+            </div>
+            <div className="image content">
+              <div className="image">
+                <i className="archive icon"></i>
+              </div>
+              <div className="description">
+                <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+              </div>
+            </div>
+            <div className="actions">
+              <div className="two fluid ui inverted buttons">
+                <div className="ui cancel red basic inverted button">
+                  <i className="remove icon"></i>
+                  No
+                </div>
+                <div className="ui ok green basic inverted button">
+                  <i className="checkmark icon"></i>
+                  Yes
+                </div>
+              </div>
+            </div>
         </Modal>
       </div>
     );
--- assets/tasks/modal/modal.jsx
+++ assets/tasks/modal/modal.jsx
@@ -2,48 +2,27 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isShown: false
-    };
+    this.showModal = this.showModal().bind(this);
+  }
+
+  showModal(){
+    let result;
+    if(this.props.isShown){
+      result = <div className="ui basic active modal">
+                  {this.props.children}
+                </div>
+    }else{
+      result = <div className="ui basic modal">
+                {this.props.children}
+              </div>
+    }
+    return result;
   }
 
   render() {
-    return (
-      <div className="ui basic modal">
-        <i className="close icon"></i>
-        <div className="header">
-          Archive Old Messages
-        </div>
-        <div className="image content">
-          <div className="image">
-            <i className="archive icon"></i>
-          </div>
-          <div className="description">
-            <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
-          </div>
-        </div>
-        <div className="actions">
-          <div className="two fluid ui inverted buttons">
-            <div className="ui cancel red basic inverted button">
-              <i className="remove icon"></i>
-              No
-            </div>
-            <div className="ui ok green basic inverted button">
-              <i className="checkmark icon"></i>
-              Yes
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return (<div>{this.showModal()}</div>);
   }
 }
-
-
-Modal.propTypes = {
-  isShown: React.PropTypes.boolean
-};
-
 
 // Do not use it in real applications. Use ES6 modules instead.
 window.Modal = Modal;
